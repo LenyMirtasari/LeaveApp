b49b54a5e6d350879ce85a98c196ab88a0f6efb8
LenyMirtasari <lennymirtasari@gmail.com>
2021-11-23 17:06:06 +0700

    add LeaveClient

---
+++ LeaveAPI/LeaveClient/Controllers/HistoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveClient.Controllers
+{
+    public class HistoryController : Controller
+    {
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
